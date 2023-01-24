2df62f2c80ea5f66fb9611d0818d1bea80c45963
IvanLink-py <ostaninivan8@gmail.com>
2023-01-25 00:27:41 +0500

    Структура парсера характеристик продукта

---
+++ GetProductsDetails.py
@@ -0,0 +1,26 @@
+import sys
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from pprint import pp
+import openpyxl
+
+
+def get_ref(index):
+    pass
+
+
+def get_details_by_ref(ref):
+    pass
+
+
+def write_details(details):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
