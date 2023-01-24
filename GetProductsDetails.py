010fa498944ca3339448ac39bb6dda433e5dc28a
IvanLink-py <ostaninivan8@gmail.com>
2023-01-25 00:33:33 +0500

    Реализация получения ссылки на продукт из таблицы

--- GetProductsDetails.py
+++ GetProductsDetails.py
@@ -6,8 +6,22 @@ from pprint import pp
 import openpyxl
 
 
-def get_ref(index):
-    pass
+def get_ref(workbook, sheet_name):
+    start = 4
+    index = 0
+
+    wb = openpyxl.load_workbook(workbook)
+    sheet = wb[sheet_name]
+
+    while True:
+        value = sheet[f"B{start + index}"].value
+
+        if value is None:
+            return
+
+        yield index, value
+
+        index += 1
 
 
 def get_details_by_ref(ref):
@@ -19,7 +33,8 @@ def write_details(details):
 
 
 def main():
-    pass
+    for i, ref in get_ref("Export.xlsx", "Export"):
+        print(i, ref)
 
 
 if __name__ == '__main__':
